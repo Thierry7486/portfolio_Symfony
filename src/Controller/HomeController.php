c8040373632eff9b54c5e70a6ff008e8ab7d201f
Thierry7486 <thierrymalherbe@live.fr>
2021-09-21 20:07:08 +0200

    Mise en place d'un contrôleur pour afficher 3 skills au hasard

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,12 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Skill;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
 
 	public function indexAction() {
-		return $this->render('home.html.twig');
+
+		// aller récupérer 3 skills au piff
+		$skills = $this->getDoctrine()->getRepository(Skill::class)->findAll();
+		// je randomise ma liste de compétences
+		shuffle($skills);
+		// je cut le tableau pour garder les 3 premiers
+		$skills = array_slice($skills, 0,3);
+		
+
+		return $this->render('home.html.twig', 
+		['skills' => $skills]);
 	}
 }
